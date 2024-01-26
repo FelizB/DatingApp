6305af6d3be2f30a4bd2bc11db21df453fb42d73
Felix <felantezb@gmail.com>
2024-01-27 00:15:32 +0300

    Controller setup

---
+++ Controllers/UsersContoller.cs
@@ -0,0 +1,33 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+[ApiController]
+[Route("api/users")]  //  /api/users
+public class UsersContoller : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersContoller(DataContext context)
+    {
+        _context = context;
+    }
+
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _context.Users.ToListAsync();
+        return users;
+    }
+
+    [HttpGet("{id}")] // /api/users/2
+    public async Task<ActionResult<AppUser>> GetUsers(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+}
